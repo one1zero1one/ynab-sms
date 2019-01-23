819fa77303da565a0333c54b743f309d53cb98d8
Daniel Radu <daniel.radu@sentiance.com>
2019-01-23 01:16:44 +0000

    first commit - added slack support, structured logging, scheduled run and Dockerfile

--- README.md
+++ README.md
@@ -1,23 +1,52 @@
 # Intro
-Do you use YNAB with mutliple people? use `ynab-sms.py` to send text messages to other users of your budget to keep everyone in the loop! The script uses flag colors to identify who's transaction it is. For instance, I mark my transactions with a blue flag, and my wife marks hers with a purple flag. The script will also alert all users if a category's budgeted amount gets changed.
-# Getting started
+
+Do you use YNAB with mutliple people? use `ynab-sms.py` to send text or messages to other users of your budget (or/and to a slack channel) to keep everyone in the loop! The script uses flag colors to identify who's transaction it is. For instance, I mark my transactions with a blue flag, and my wife marks hers with a purple flag. The script will also alert all users if a category's budgeted amount gets changed.
+
+## Getting started
+
 ## Install Requirements
+
 The script requires python 3. Install the required python packages with `pip install -r requirements.txt`
+
 ## Configure
-Copy `settings.json.example` to `settings.json` and then modify with your settings. You'll need a Personal Access Token from your [YNAB Developer Settings](https://app.youneedabudget.com/settings/developer), and a [Twilio](https://www.twilio.com/sms) Programmable SMS account.
-#### Sample Config
-The configuration is broken up into three sections.
+
+Copy `settings.json.example` to `settings.json` and then modify with your settings. You'll need a Personal Access Token from your [YNAB Developer Settings](https://app.youneedabudget.com/settings/developer),a [Twilio](https://www.twilio.com/sms) Programmable SMS account for getting notified via sms and/or a [Slack](https://api.slack.com/custom-integrations/legacy-tokens) (legacy) Token for getting notification on a slack channel.
+
+### Sample Config
+
+The configuration is broken up into six sections.
+
+In the `notify` section you need to select the notification methods. In below example, both methods are enabled.
+
+```json
+ "notify": ["slack", "twilio"]
+```
+
+In the `frequency` section you need to set how often the script will run (in _minutes_). Keep in mind the YNAB API call limits, which are currently 200 calls per hour, and the script makes three calls per run. Default config is once every 10 minutes (18 calls).
+
+```json
+ "frequency": 10
+```
+
+In the `currency` section, setup your desired coin sign.
+
+```json
+"coin": "EUR"
+```
 
 In the `ynab` section you'll need to fill in your budget ID, which can be found in the url bar when you're editing your budget: `https://app.youneedabudget.com/THIS IS YOUR BUDGET ID/budget`, as well as a Personal Access Token from your YNAB Developer Settings.
 
 (The information below is all random)
+
 ```json
     "ynab": {
         "budget": "h1347489-2b23-6dcf-feb9-6204ba34a4fe",
         "token": "44b913a04a2f3c898d498a22b34a236205664ba97d68585446dcff6c04feb6"
     },
 ```
+
 In the `twilio` section you'll need your Account SID, auth token, and your programmable sms number.
+
 ```json
     "twilio": {
         "sid": "ee5a1a322e6415d68cAC4fef0bf61899ed",
@@ -25,15 +54,26 @@ In the `twilio` section you'll need your Account SID, auth token, and your progr
         "number": "+12135552560"
     },
 ```
+
+In the `slack` section, you'll need your API token, and the chanel you want to send the notifications to.
+
+```json
+ "slack:": {
+        "token": "xoxp-xxx-yyy-zzz-aaa",
+        "channel": "#finance"
+    }
+```
+
 Finally, in the `users` section add each person you want to get SMS alerts, and the flag color for the transactions they enter.
+
 ```json
     "users": [
-        {            
+        {
             "name": "Alice",
             "number": "+16615557189",
             "flag": "purple"
         },
-        {            
+        {
             "name": "Bob",
             "number": "+13235557167",
             "flag": "blue"
@@ -41,10 +81,19 @@ Finally, in the `users` section add each person you want to get SMS alerts, and 
     ]
 }
 ```
-## Run
-Once configured, run `python3 ynab-sms.py` using cron or some other task scheduler as often as you want. Keep in mind the YNAB API call limits, which are currently 200 calls per hour, and the script makes three calls per run.
 
-The script writes a couple json files to the local directory to keep track of past data, so make sure it has write permissions.
+## Run as a script
 
-# Improvements
+Once configured, run `python3 ynab-sms.py`. The script writes a couple json files to the local directory to keep track of past data, so make sure it has write permissions.
+
+## Run in docker
+
+Build with `docker build --tag ynab-sms .` and run with `docker run ynab-sms`.
+
+## Improvements
+
 Currently this script grabs all possible transactions. A future version should use the delta api instead. The payee list should be cached as well.
+
+## Credits
+
+Kudos to [ljb2of3](https://github.com/ljb2of3) for doing all the heavy lifting (calculation of things and main logic), I have forked and butchered the code to fit my needs.
--- ynab-sms.py
+++ ynab-sms.py
@@ -1,127 +1,190 @@
 from twilio.rest import Client
+from slackclient import SlackClient
+from structlog import get_logger
+import schedule
+import time
 import requests
 import json
 
+# Init logger
+log = get_logger()
 
-# Load config data
+# Load config data once, on startup
 try:
 	with open('settings.json', 'r') as f:
 		settings = json.load(f)
+		log.msg("Loading config on startup", file=f)
 except:
-	print("Error loading settings. Verify that settings.json exists and contains valid json!")
+	log.Error("Error loading settings. Verify that settings.json exists and contains valid json!", file=f)
 	exit(-1)
 
+# Fill flags from config
 flags = {}
-
 for user in settings['users']:
 	flags[user['flag']] = user['name']
 
-# Load previous transactions
-try:
-	with open('known-transactions.json', 'r') as f:
-		knownTransactions = json.load(f)
-except:
-	knownTransactions = []
+# Init Slack from config - TODO when slack is in notification list, check if token and channel defined.
+sc = SlackClient(settings['slack']['token'])
 
-
-# Load previous budget amounts
-try:
-	with open('budgeted-amounts.json', 'r') as f:
-		budgetedAmounts = json.load(f)
-except:
-	budgetedAmounts = {}
-
-
-def sendSMS(who, message):
-	print('To: ' + who)
-	print('  ' + message)
-	print()
-
+def sendSMS(number, message):
 	twilio = Client(settings['twilio']['sid'], settings['twilio']['token'])
-	twilio.messages.create(to=who, from_=settings['twilio']['number'], body=message)
+	twilio.messages.create(to=number, from_=settings['twilio']['number'], body=message)
+	log.msg("SMS message", to=number, from_=settings['twilio']['number'], body=message)
 
+def sendSlack(message):
+	sc.api_call("chat.postMessage", channel=settings['slack']['channel'], text=message)
+	log.msg("Slack message", channel=settings['slack']['channel'], text=message)
 
-def processTransaction(id, doc, balance):
+def processTransaction(id, doc, balance, knownTransactions):
 	if id not in knownTransactions:
 		knownTransactions.append(id)
-		if doc['flag'] in flags and doc['outflow'] > 0:			
-			for user in settings['users']:
-				if user['flag'] not in doc['flag']:
-					text = flags[doc['flag']] + ' spent $' + '{:01.2f}'.format(doc['outflow']) + ' @ ' + doc['payee'] + ' from ' + doc['category'] + '. $' + '{:01.2f}'.format(balance) + ' remaining. (' + doc['memo'] + ')'
-					sendSMS(user['number'], text)
-
-
-# Grab the current data from the YNAB API
-headers = {
-	'accept': 'application/json',
-	'Authorization': 'Bearer ' + settings['ynab']['token']
-}
-
-ynabURL = 'https://api.youneedabudget.com/v1'
-budgetURL = ynabURL + '/budgets/' + settings['ynab']['budget']
-
-data = None
-
-r = requests.get(budgetURL + '/transactions', headers=headers)
-r.raise_for_status()
-data = r.json()
-
-transactions = data['data']['transactions']
-
-# Get the Category list
-r = requests.get(budgetURL + '/categories', headers=headers)
-r.raise_for_status()
-data = r.json()
-
-categories = {}
-for g in data['data']['category_groups']:
-	for c in g['categories']:
-		if c['id'] in budgetedAmounts:
-			if c['budgeted'] != budgetedAmounts[c['id']]:				
-				if c['budgeted'] > budgetedAmounts[c['id']]: 
-					amount = (c['budgeted'] - budgetedAmounts[c['id']]) / 1000.0
-					text = 'The amount budgeted for ' + c['name'] + ' has increased by $' + '{:01.2f}'.format(amount) + '. There is now $' + '{:01.2f}'.format(c['balance'] / 1000.0) + ' remaining.'
-				else:
-					amount = (budgetedAmounts[c['id']] - c['budgeted']) / 1000.0
-					text = 'The amount budgeted for ' + c['name'] + ' has decreased by $' + '{:01.2f}'.format(amount) + '. There is now $' + '{:01.2f}'.format(c['balance'] / 1000.0) + ' remaining.'
+		if doc['flag'] in flags and doc['outflow'] > 0:
+			#SMS
+			if 'twilio' in settings['notify']:			
 				for user in settings['users']:
-					sendSMS(user['number'], text)
-		
-		budgetedAmounts[c['id']] = c['budgeted']
-		
-		categories[c['id']] = {}
-		categories[c['id']]['name'] = c['name']
-		categories[c['id']]['balance'] = c['balance'] / 1000.0
-		categories[c['id']]['budgeted'] = c['budgeted'] / 1000.0
-		categories[c['id']]['group'] = g['name']
+					if user['flag'] not in doc['flag']:
+						text = flags[doc['flag']] + ' spent ' + settings['coin'] + '{:01.2f}'.format(doc['outflow']) + ' @ ' + doc['payee'] + ' from ' + doc['category'] + ' in account ' + doc['account'] + '. '  + settings['coin'] + '{:01.2f}'.format(balance) + ' remaining. (' + doc['memo'] + ')'
+						sendSMS(user['number'], text)
+			#Slack
+			if 'slack' in settings['notify']:
+				text = flags[doc['flag']] + ' spent ' + settings['coin'] + '{:01.2f}'.format(doc['outflow']) + ' @ ' + doc['payee'] + ' from ' + doc['category'] + ' in account ' + doc['account'] + '. ' + settings['coin'] + '{:01.2f}'.format(balance) + ' remaining. (' + doc['memo'] + ')'
+				sendSlack(text)
 
-# Get the Payee list
-r = requests.get(budgetURL + '/payees', headers=headers)
-r.raise_for_status()
-data = r.json()
+def job():
+	log.msg("Starting job")
+	# Load previous transactions
+	try:
+		with open('known-transactions.json', 'r') as f:
+			knownTransactions = json.load(f)
+			log.msg("Loading known transactions", file=f)
+	except:
+		knownTransactions = []
+		log.msg("No known transactions", file=f)
 
-payees = {}
-for p in data['data']['payees']:
-	payees[p['id']] = p['name']
+	# Load previous budget amounts
+	try:
+		with open('budgeted-amounts.json', 'r') as f:
+			budgetedAmounts = json.load(f)
+			log.msg("Loading previous bugeted amounts", file=f)
+	except:
+		budgetedAmounts = {}
+		log.msg("No previous bugeted amounts", file=f)
 
-# Process Transactions
-for t in transactions:
-	# Check to see if this is a split transaction
-	if len(t['subtransactions']) > 0:
-		for s in t['subtransactions']:
-			id = s['id']			
+	# Grab the current data from the YNAB API
+	headers = {
+		'accept': 'application/json',
+		'Authorization': 'Bearer ' + settings['ynab']['token']
+	}
+
+	ynabURL = 'https://api.youneedabudget.com/v1'
+	budgetURL = ynabURL + '/budgets/' + settings['ynab']['budget']
+
+	data = None
+
+	r = requests.get(budgetURL + '/transactions', headers=headers)
+	r.raise_for_status()
+	log.msg("Got /transactions", duration=r.elapsed.microseconds, size=len(r.content))
+
+	data = r.json()	
+	transactions = data['data']['transactions']
+
+	# Get the Category list
+	r = requests.get(budgetURL + '/categories', headers=headers)
+	r.raise_for_status()
+	log.msg("Got /categories", duration=r.elapsed.microseconds, size=len(r.content))
+	
+	data = r.json()
+	
+	categories = {}
+	for g in data['data']['category_groups']:
+		for c in g['categories']:
+			if c['id'] in budgetedAmounts:
+				if c['budgeted'] != budgetedAmounts[c['id']]:				
+					if c['budgeted'] > budgetedAmounts[c['id']]: 
+						amount = (c['budgeted'] - budgetedAmounts[c['id']]) / 1000.0
+						text = 'The amount budgeted for ' + c['name'] + ' has increased by ' + settings['coin'] + '{:01.2f}'.format(amount) + '. There is now ' + settings['coin'] + '{:01.2f}'.format(c['balance'] / 1000.0) + ' remaining.'
+					else:
+						amount = (budgetedAmounts[c['id']] - c['budgeted']) / 1000.0
+						text = 'The amount budgeted for ' + c['name'] + ' has decreased by '+ settings['coin'] + '{:01.2f}'.format(amount) + '. There is now ' + settings['coin'] + '{:01.2f}'.format(c['balance'] / 1000.0) + ' remaining.'
+					#SMS
+					if 'twilio' in settings['notify']:			
+						for user in settings['users']:
+							sendSMS(user['number'], text)
+					if 'slack' in settings['notify']:
+						sendSlack(text)
+
+			budgetedAmounts[c['id']] = c['budgeted']
+			
+			categories[c['id']] = {}
+			categories[c['id']]['name'] = c['name']
+			categories[c['id']]['balance'] = c['balance'] / 1000.0
+			categories[c['id']]['budgeted'] = c['budgeted'] / 1000.0
+			categories[c['id']]['group'] = g['name']
+
+	# Get the Payee list
+	r = requests.get(budgetURL + '/payees', headers=headers)
+	r.raise_for_status()
+	log.msg("Got /payees", duration=r.elapsed.microseconds, size=len(r.content))
+	
+	data = r.json()
+
+	payees = {}
+	for p in data['data']['payees']:
+		payees[p['id']] = p['name']
+
+	# Process Transactions
+	for t in transactions:
+		# Check to see if this is a split transaction
+		if len(t['subtransactions']) > 0:
+			for s in t['subtransactions']:
+				id = s['id']			
+				doc = {}
+				doc['date'] = t['date']
+				doc['amount'] = s['amount'] / 1000.0
+				if doc['amount'] < 0:
+					doc['outflow'] = doc['amount'] * -1
+					doc['inflow'] = 0
+				else:
+					doc['outflow'] = 0
+					doc['inflow'] = doc['amount']
+				if s['memo'] is not None:
+					doc['memo'] = s['memo']
+				elif t['memo'] is not None:
+					doc['memo'] = t['memo']
+				else:
+					doc['memo'] = ''
+				doc['status'] = t['cleared']
+				doc['approved'] = t['approved']
+				doc['flag'] = t['flag_color']
+				doc['account'] = t['account_name']
+				if s['payee_id'] is not None:
+					doc['payee'] = payees[s['payee_id']]
+				elif t['payee_id'] is not None:
+					doc['payee'] = payees[t['payee_id']]
+				doc['category'] = None
+				doc['group'] = None
+				catBalance = None
+				if s['category_id'] is not None:
+					doc['category'] = categories[s['category_id']]['name']
+					doc['group'] = categories[s['category_id']]['group']
+					catBalance = categories[s['category_id']]['balance']
+				elif t['category_id'] is not None:
+					doc['category'] = categories[t['category_id']]['name']
+					doc['group'] = categories[t['category_id']]['group']
+					catBalance = categories[t['category_id']]['balance']            
+				processTransaction(id, doc, catBalance, knownTransactions)
+		else:
+			id = t['id']
 			doc = {}
 			doc['date'] = t['date']
-			doc['amount'] = s['amount'] / 1000.0
+			doc['amount'] = t['amount'] / 1000.0
 			if doc['amount'] < 0:
 				doc['outflow'] = doc['amount'] * -1
 				doc['inflow'] = 0
 			else:
 				doc['outflow'] = 0
 				doc['inflow'] = doc['amount']
-			if s['memo'] is not None:
-				doc['memo'] = s['memo']
-			elif t['memo'] is not None:
+			if t['memo'] is not None:
 				doc['memo'] = t['memo']
 			else:
 				doc['memo'] = ''
@@ -129,54 +192,30 @@ for t in transactions:
 			doc['approved'] = t['approved']
 			doc['flag'] = t['flag_color']
 			doc['account'] = t['account_name']
-			if s['payee_id'] is not None:
-				doc['payee'] = payees[s['payee_id']]
-			elif t['payee_id'] is not None:
+			if t['payee_id'] is not None:
 				doc['payee'] = payees[t['payee_id']]
 			doc['category'] = None
 			doc['group'] = None
 			catBalance = None
-			if s['category_id'] is not None:
-				doc['category'] = categories[s['category_id']]['name']
-				doc['group'] = categories[s['category_id']]['group']
-				catBalance = categories[s['category_id']]['balance']
-			elif t['category_id'] is not None:
+			if t['category_id'] is not None:
 				doc['category'] = categories[t['category_id']]['name']
 				doc['group'] = categories[t['category_id']]['group']
-				catBalance = categories[t['category_id']]['balance']            
-			processTransaction(id, doc, catBalance)
-	else:
-		id = t['id']
-		doc = {}
-		doc['date'] = t['date']
-		doc['amount'] = t['amount'] / 1000.0
-		if doc['amount'] < 0:
-			doc['outflow'] = doc['amount'] * -1
-			doc['inflow'] = 0
-		else:
-			doc['outflow'] = 0
-			doc['inflow'] = doc['amount']
-		if t['memo'] is not None:
-			doc['memo'] = t['memo']
-		else:
-			doc['memo'] = ''
-		doc['status'] = t['cleared']
-		doc['approved'] = t['approved']
-		doc['flag'] = t['flag_color']
-		doc['account'] = t['account_name']
-		if t['payee_id'] is not None:
-			doc['payee'] = payees[t['payee_id']]
-		doc['category'] = None
-		doc['group'] = None
-		catBalance = None
-		if t['category_id'] is not None:
-			doc['category'] = categories[t['category_id']]['name']
-			doc['group'] = categories[t['category_id']]['group']
-			catBalance = categories[t['category_id']]['balance']
-		processTransaction(id, doc, catBalance)
+				catBalance = categories[t['category_id']]['balance']
+			processTransaction(id, doc, catBalance, knownTransactions)
 
-with open('known-transactions.json', 'w') as f:
-	json.dump(knownTransactions, f, indent=4, sort_keys=True, ensure_ascii = False)
+	with open('known-transactions.json', 'w') as f:
+		json.dump(knownTransactions, f, indent=4, sort_keys=True, ensure_ascii = False)
+		log.msg("Saving transactions", file=f)
 
-with open('budgeted-amounts.json', 'w') as f:
-	json.dump(budgetedAmounts, f, indent=4, sort_keys=True, ensure_ascii = False)
+	with open('budgeted-amounts.json', 'w') as f:
+		json.dump(budgetedAmounts, f, indent=4, sort_keys=True, ensure_ascii = False)
+		log.msg("Saving budgeted amounts", file=f)
+
+# schedule the job - TODO check if frequency defined
+schedule.every(settings['frequency']).minutes.do(job)
+log.msg("Scheduling job", frequency=settings['frequency'])
+
+# run
+while True:
+	schedule.run_pending()
+	time.sleep(1)
